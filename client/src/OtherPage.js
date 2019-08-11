048b0be1f50dd08df249a4f7b40214702af1ae81
Rachel Lin <rachel.lynch.lin@gmail.com>
2019-08-11 12:02:03 -0700

    adjusted the layout of elements on both pages

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <div>
-      Im some other page
+      <p>I'm some other page </p>
       <Link to="/">Go back to home page!</Link>
     </div>
   );
